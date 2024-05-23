a955be50eecf2befba5233ece74cea90ac5db8a3
mnlyuzina <luzinamari@gmail.com>
2024-05-23 16:11:15 +0300

    fix menu page

--- static/javascripts/counter.js
+++ static/javascripts/counter.js
@@ -2,18 +2,20 @@ function addHandlers(count) {
   var minus = count.querySelector(".minus");
   var number = count.querySelector(".counter-value");
   var plus = count.querySelector(".plus");
+
   plus.addEventListener("click", function() {
-    if (number.innerText == 10) {
-        number.innerText = 10;
+    if (number.value == 10) {
+        number.value = 10;
     } else {
-        number.innerText++;
+        number.value++;
     }
   });
+
   minus.addEventListener("click", function() {
-    if (number.innerText <= 1) {
-        number.innerText = 1;
+    if (number.value <= 1) {
+        number.value = 1;
     } else {
-        number.innerText--;
+        number.value--;
     }
   });
 }
